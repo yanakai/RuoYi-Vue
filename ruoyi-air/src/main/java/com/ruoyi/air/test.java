515488ffb1531cc4e292cbbc3573d1353324a452
yankai <yanakai@126.com>
2023-04-11 16:32:31 +0800

    增加决策子平台和协同子平台模块代码

---
+++ ruoyi-air/src/main/java/com/ruoyi/air/test.java
@@ -0,0 +1,12 @@
+package com.ruoyi.air;
+
+/**
+ * @BelongsProject: RuoYi-Vue
+ * @BelongsPackage: com.ruoyi.air
+ * @Author: yanakai@126.com
+ * @CreateTime: 2023-04-11  16:09
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class test {
+}
---
+++ ruoyi-coordination/src/main/java/com/ruoyi/coordination/test.java
@@ -0,0 +1,12 @@
+package com.ruoyi.coordination;
+
+/**
+ * @BelongsProject: RuoYi-Vue
+ * @BelongsPackage: com.ruoyi.coordination
+ * @Author: yanakai@126.com
+ * @CreateTime: 2023-04-11  16:10
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class test {
+}
