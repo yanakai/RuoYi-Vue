4040979e016dd46d89fc76bcad0d560d01464745
yankai <yanakai@126.com>
2024-05-13 13:32:45 +0800

    修改

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -36,7 +36,7 @@ module.exports = {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
         //target: `http://localhost:9090`,
-        target: `http://127..0.0.1:9090`,
+        target: `http://127.0.0.0.1:9090`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
