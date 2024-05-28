611f63ee17b2fd8e4871c7047794264f1dd1802d
yankai <yanakai@126.com>
2024-05-28 09:48:38 +0800

    修改包名

---
+++ mapuni-common/src/main/java/com/yutu/common/config/RuoYiConfig.java
@@ -0,0 +1,173 @@
+package com.yutu.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 读取项目相关配置
+ *
+ * @author ruoyi
+ */
+@Component
+@ConfigurationProperties(prefix = "ruoyi")
+public class RuoYiConfig
+{
+    /** 项目名称 */
+    private String name;
+
+    /** 版本 */
+    private String version;
+
+    /** 版权年份 */
+    private String copyrightYear;
+
+    /** 上传路径 */
+    private static String profile;
+
+    /** 获取地址开关 */
+    private static boolean addressEnabled;
+
+    /** 验证码类型 */
+    private static String captchaType;
+
+    /** 日志远程上传开关 */
+    private static boolean originLogEnabled;
+    /** 子系统唯一id 数据从统一权限菜单列表中获取 */
+    private static Long systemId;
+    /** 子系统唯一key 数据从统一权限菜单列表中获取 */
+    private static String systemKey;
+    /** 子系统名称 数据从统一权限菜单列表中获取 */
+    private static String systemName;
+    /** 统一权限路径 */
+    private static String ssoUrl;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+    public void setVersion(String version)
+    {
+        this.version = version;
+    }
+
+    public String getCopyrightYear()
+    {
+        return copyrightYear;
+    }
+
+    public void setCopyrightYear(String copyrightYear)
+    {
+        this.copyrightYear = copyrightYear;
+    }
+
+    public static String getProfile()
+    {
+        return profile;
+    }
+
+    public void setProfile(String profile)
+    {
+        RuoYiConfig.profile = profile;
+    }
+
+    public static boolean isAddressEnabled()
+    {
+        return addressEnabled;
+    }
+
+    public void setAddressEnabled(boolean addressEnabled)
+    {
+        RuoYiConfig.addressEnabled = addressEnabled;
+    }
+
+    public static String getCaptchaType() {
+        return captchaType;
+    }
+
+    public void setCaptchaType(String captchaType) {
+        RuoYiConfig.captchaType = captchaType;
+    }
+
+    public static String getSsoUrl() {
+        return ssoUrl;
+    }
+
+    public void setSsoUrl(String ssoUrl) {
+        this.ssoUrl = ssoUrl;
+    }
+
+    public static boolean isOriginLogEnabled() {
+        return originLogEnabled;
+    }
+
+    public void setOriginLogEnabled(boolean originLogEnabled) {
+        RuoYiConfig.originLogEnabled = originLogEnabled;
+    }
+
+    public static Long getSystemId() {
+        return systemId;
+    }
+
+    public void setSystemId(Long systemId) {
+        this.systemId = systemId;
+    }
+
+    public static String getSystemKey() {
+        return systemKey;
+    }
+
+    public void setSystemKey(String systemKey) {
+        this.systemKey = systemKey;
+    }
+
+    public static String getSystemName() {
+        return systemName;
+    }
+
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
+
+    /**
+     * 获取导入上传路径
+     */
+    public static String getImportPath()
+    {
+        return getProfile() + "/import";
+    }
+
+    /**
+     * 获取头像上传路径
+     */
+    public static String getAvatarPath()
+    {
+        return getProfile() + "/avatar";
+    }
+
+    /**
+     * 获取下载路径
+     */
+    public static String getDownloadPath()
+    {
+        return getProfile() + "/download/";
+    }
+
+    /**
+     * 获取上传路径
+     */
+    public static String getUploadPath()
+    {
+        return getProfile() + "/upload";
+    }
+}
---
+++ mapuni-common/src/main/java/com/yutu/common/utils/http/HttpRequest.java
@@ -0,0 +1,64 @@
+package com.yutu.common.utils.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Description: http 请求工具类
+ * @Author: yanakai@126.com
+ * @CreateDate: 2024-02-23 15:11
+ * @UpdateUser: yanakai@126.com
+ * @UpdateDate: 2024-02-23 15:11
+ * @UpdateRemark: 修改内容
+ * @Version: 1.0
+ */
+public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+
+    /**
+     * @title post
+     * @description  http请求 用户参数在body中传参
+     * @param httpUrl  请求地址
+     * @param paramsObject  参数对象
+     * @return java.lang.String
+     * @author yanakai@126.com
+     * @date   2024-02-23
+     */
+    public static String post(String httpUrl, String paramsObject,String token) throws IOException {
+        URL url = new URL(httpUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization","Bearer "+token);
+        connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setDoOutput(true);
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = paramsObject.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+
+        int code = connection.getResponseCode();
+        if (code != HttpURLConnection.HTTP_OK) {
+            throw new IOException("Failed : HTTP error code : " + code + " for URL : " + httpUrl);
+        }
+
+        // 如果需要处理响应内容
+        try (InputStream is = connection.getInputStream();
+             InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+             BufferedReader br = new BufferedReader(isr)) {
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+            return response.toString();
+        }
+    }
+
+}
