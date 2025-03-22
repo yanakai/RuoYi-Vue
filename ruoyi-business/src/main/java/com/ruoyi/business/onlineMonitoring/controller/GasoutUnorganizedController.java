823f754b605fb872f79c77f43ca216a49a62bb47
liuxinxin <liuxinxin@baicgroup.com.cn>
2025-03-22 16:21:51 +0800

    feat(business): 添加废气排口在线监测功能
    - 新增 GasoutUnorganizedController 控制器- 实现扬尘排口和 VOC 排口的在线监测列表查询
    - 添加废气排口在线监测数据导出功能
    - 异常情况下返回统一错误信息

---
+++ ruoyi-business/src/main/java/com/ruoyi/business/onlineMonitoring/controller/GasoutUnorganizedController.java
@@ -0,0 +1,90 @@
+package com.ruoyi.business.onlineMonitoring.controller;
+
+import com.ruoyi.business.onlineMonitoring.dto.GasoutDTO;
+import com.ruoyi.business.statistics.service.IGasoutService;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.constant.HttpStatus;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 废气排口在线监测 Controller
+ *
+ * @author lx
+ * @date 2024-07-04
+ */
+@Slf4j
+@Api(value = "业务模块-废气排口在线监测", tags = "废气排口在线监测")
+@RestController
+@RequestMapping("/business/onlinemonitoring/gasoutPutUnorganized")
+public class GasoutUnorganizedController extends BaseController {
+    @Autowired
+    private IGasoutService gasoutService;
+
+    /**
+     * 扬尘排口在线监测
+     */
+    @ApiOperation("废气排口在线监测")
+    @GetMapping("/yc/list")
+    public TableDataInfo ycList(GasoutDTO gasoutDTO) {
+         try {
+             return gasoutService.selectDataList(gasoutDTO);
+         }catch (Exception e){
+             log.error("排口查询错误",e);
+             TableDataInfo tableDataInfo = new TableDataInfo();
+             tableDataInfo.setRows(null);
+             tableDataInfo.setTotal(0);
+             tableDataInfo.setCode(HttpStatus.NO_CONTENT);
+             tableDataInfo.setMsg("操作已经执行成功，但是没有返回数据");
+             return  tableDataInfo;
+         }
+
+    }
+
+    /**
+     * VOC排口在线监测
+     */
+    @ApiOperation("废气排口在线监测")
+    @GetMapping("/voc/list")
+    public TableDataInfo vocList(GasoutDTO gasoutDTO) {
+        try {
+            return gasoutService.selectDataList(gasoutDTO);
+        }catch (Exception e){
+            log.error("排口查询错误",e);
+            TableDataInfo tableDataInfo = new TableDataInfo();
+            tableDataInfo.setRows(null);
+            tableDataInfo.setTotal(0);
+            tableDataInfo.setCode(HttpStatus.NO_CONTENT);
+            tableDataInfo.setMsg("操作已经执行成功，但是没有返回数据");
+            return  tableDataInfo;
+        }
+
+    }
+
+    /**
+     * 废气排口在线监测导出
+     */
+    @ApiOperation("废气排口在线监测导出")
+    @Log(title = "废气排口在线监测导出", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, GasoutDTO gasoutDTO) {
+        try {
+            gasoutService.export(response,gasoutDTO);
+        }catch (Exception e){
+            log.error("排口导出错误",e);
+        }
+    }
+
+
+}
