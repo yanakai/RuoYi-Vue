0d5ecbe8799119c9bc909cf9877335451b6ca760
wugl <bjqy-wugl@glodon.com>
2025-07-02 09:50:02 +0800

    设置分页参数判断

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/PageUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/PageUtils.java
@@ -1,14 +1,17 @@
 package com.ruoyi.common.utils;
 
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.ruoyi.common.constant.HttpStatus;
 import com.ruoyi.common.core.page.PageDomain;
+import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.core.page.TableSupport;
 import com.ruoyi.common.utils.sql.SqlUtil;
 
+import java.util.List;
+
 /**
  * 分页工具类
- *
- * @author ruoyi
  */
 public class PageUtils extends PageHelper {
     /**
@@ -24,9 +27,45 @@ public class PageUtils extends PageHelper {
     }
 
     /**
+     * 设置请求分页数据
+     * 无分页参数时不分页
+     * @return false:未分页
+     */
+    public static boolean startPageCheckExists() {
+        PageDomain pageDomain = TableSupport.getPageDomain(null, null);
+        if (null == pageDomain.getPageNum() || null == pageDomain.getPageSize()) {
+            return false;
+        }
+        Integer pageNum = pageDomain.getPageNum();
+        Integer pageSize = pageDomain.getPageSize();
+        String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
+        Boolean reasonable = pageDomain.getReasonable();
+        PageHelper.startPage(pageNum, pageSize, orderBy).setReasonable(reasonable);
+        return true;
+    }
+
+    /**
      * 清理分页的线程变量
      */
     public static void clearPage() {
         PageHelper.clearPage();
+    }
+
+    /**
+     * 响应请求分页数据
+     */
+    public static TableDataInfo getDataTable(List<?> list, boolean page) {
+        TableDataInfo rspData = new TableDataInfo();
+        rspData.setCode(HttpStatus.SUCCESS);
+        rspData.setMsg("查询成功");
+        rspData.setRows(list);
+        if (page) {
+            rspData.setTotal(new PageInfo<>(list).getTotal());
+            // 清理分页参数
+            clearPage();
+        } else {
+            rspData.setTotal(list.size());
+        }
+        return rspData;
     }
 }
